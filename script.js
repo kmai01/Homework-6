239795e97aa7acbaf4bc876c0d0c44416a335249
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-02-04 02:58:55 -0600

    add weather data

--- script.js
+++ script.js
@@ -19,8 +19,38 @@ $("#searchButton").click(function (event) {
     method: 'GET'
     }).then(function (response) {
     console.log(response)
+
+    // get date from response
+    let unixTimestamp=response.dt
+    console.log(unixTimestamp); 
+
+     // convert unix time
+    var date = new Date(unixTimestamp * 1000)
+    console.log(date);
+    var ndate=moment(date).format('MM/DD/YYYY')
+    console.log(ndate);
+   
+    // Get a city
+
+    $(".city-date").text(response.name+" "+"("+ndate+")"+" "+response.weather[0].icon)
+    
+    // Get temperature and convert to F°
+    var tempF = (response.main.temp-273.15)*1.80 +32;
+        console.log(tempF);
+    var tempFd= tempF.toFixed(1);
+    $(".temp").text(tempFd+" "+"°F");
+    // Get Humidity
+    var humidity=(response.main.humidity)
+    $(".humidity").text(humidity+" "+"%");
+    // Get Wind Speed
+    var wind= (response.wind.speed)*2.2369362912
+    console.log(response.wind.speed)
+    console.log(wind)
+     var windd=wind.toFixed(1);
+    $(".wind").text(windd+" "+"MPH");
         
     });
+
 
     var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="+cityname+"&appid=981be588e021507f49cbb2c9d6bdad93"
 
@@ -29,7 +59,36 @@ $("#searchButton").click(function (event) {
         method: 'GET'
         }).then(function (response) {
         console.log(response)
+
+        let unixTimestamp=response.list[7].dt
+        console.log(unixTimestamp);
+
+        var date = new Date(unixTimestamp * 1000)
+        console.log(date);
+        var ndate=moment(date).format('MM/DD/YYYY')
+        console.log(ndate);
+        $(".date1").text(ndate);
+
+        let icon=response.list[7].weather[0].icon
+        console.log(icon);
+        $(".icon").text(icon);
+
+        var tempF = (response.list[7].main.temp-273.15)*1.80 +32;
+        console.log(tempF);
+        var tempFd= tempF.toFixed(1);
+        $(".temp1").text(tempFd+" "+"°F");
+
             
         });
+
+      //  var uvURL = "https://api.openweathermap.org/data/2.5/uvi?q="+cityname+"&appid=981be588e021507f49cbb2c9d6bdad93"
+
+    // $.ajax({
+        // url: uvURL,
+       // method: 'GET'
+        // }).then(function (response) {
+       //  console.log(response)
+            
+       // });
 });
 
